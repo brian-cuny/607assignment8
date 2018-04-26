3b9b7ffc1778ada9549e634149b45e3ab14e8a9f
brian.weinfeld+cuny@gmail.com <brian.weinfeld+cuny@gmail.com>
2018-04-26 15:56:46 -0400

    added images

--- week8assignment.R
+++ week8assignment.R
@@ -1,7 +1,6 @@
 library(RMySQL)
 library(RNeo4j)
 library(tidyverse)
-library(purrrlyr)
 
 sql.password <- 'cosmic joke'
 neo4j.password <- 'asdfasdf'
@@ -44,7 +43,55 @@ summary(graph)
 
 # analysis ----------------------------------------------------------------
 
-cypher.query <- cypher(graph, 'MATCH (u:User)-[r:REVIEWS]->(m:Movie), (u2:User)-[r2:REVIEWS]->(m2:Movie) WHERE u.id <> u2.id AND m.title = m2.title AND r.rating = r2.rating RETURN u.id, u2.id, COUNT(*);')
+Recommendation <- function(id){
+  best.matches <- cypher(graph, 'MATCH (u:User {id: {id}})-[r:REVIEWS]->(m:Movie), 
+                                 (u2:User)-[r2:REVIEWS]->(m:Movie) 
+                                 WHERE u.id <> u2.id 
+                                 AND r.rating = r2.rating 
+                                 RETURN u2.id, COUNT(*) AS count;',
+                         id=id) %>%
+    filter(count == max(count)) %>%
+    select(u2.id) %>%
+    unlist() %>%
+    as.numeric()
+  if(length(best.matches) == 0){
+    print('You have no similar users!')
+    return
+  }
+  recs <- cypher(graph, 'MATCH (u2:User)-[r2:REVIEWS]->(m2:Movie)
+                         WHERE u2.id IN {best}
+                         AND NOT((:User {id:{id}})-[:REVIEWS]->(m2))
+                         RETURN m2.title, AVG(r2.rating) AS avg
+                         ORDER BY avg DESC',
+                 id = id,
+                 best = best.matches)
+  if(is.null(recs)){
+    print('Your similar users have no suggestions!')
+    return
+  }
+  print('Recommendations:')
+  print(recs)
+}
+
+my.id <- 64
+output <- Recommendation(my.id)
+
+
+cypher.query <- cypher(graph, 'MATCH (u:User)-[r:REVIEWS]->(m:Movie), 
+                       (u2:User)-[r2:REVIEWS]->(m:Movie) 
+                       WHERE u.id <> u2.id 
+                       AND r.rating = r2.rating 
+                       RETURN u.id, u2.id, COUNT(*) AS count;') %>%
+  group_by(u.id) %>%
+  filter(count == max(count)) %>%
+  arrange(u.id, u2.id)
+
+
+
+
+
+
+
 
 
 
